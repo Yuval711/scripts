40314c119390c0dd3c4e8e5cb218523294e07591
Yuval711 <Yuval711@gmail.com>
2018-01-12 12:27:50 -0500

    Update Firewall_backup_sample.py

--- Firewall_backup_sample.py
+++ Firewall_backup_sample.py
@@ -11,17 +11,14 @@ def disable_paging(remote_conn):
 
     remote_conn.send("terminal length 0\n")
     time.sleep(1)
-
     output = remote_conn.recv(1000)
-
     return output
-
 
 if __name__ == '__main__':
 
     ip = 'x.x.x.x'
-    username = 'username'
-    password = 'password'
+    username = 'fw-username'
+    password = 'fw-password'
 remote_conn_pre = paramiko.SSHClient()
 
 remote_conn_pre.set_missing_host_key_policy(
@@ -31,9 +28,7 @@ remote_conn_pre.connect(ip, username=username, password=password,)
 remote_conn = remote_conn_pre.invoke_shell()
 output = remote_conn.recv(1000)
 
-
 disable_paging(remote_conn)
-
 
 remote_conn.send("\n")
 remote_conn.send(" "+copy+" \n")
@@ -41,4 +36,3 @@ remote_conn.send("\n")
 remote_conn.send("\n")
 remote_conn.send("\n")
 time.sleep(2)
-output = remote_conn.recv(5000)
