e51e9881004cb1fc15485b55d6fd788228ff3728
Yuval711 <Yuval711@gmail.com>
2021-11-23 11:20:40 -0500

    Update CSV_add_data.py

--- CSV_add_data.py
+++ CSV_add_data.py
@@ -2,7 +2,6 @@ import csv
 
 with open('./test2.csv', 'a') as csv_file: #using 'a' for appened method
 	user3 = ('Jim','Crow','56')
-	# data = (user3)
 	add = csv.writer(csv_file)
 
 	add.writerow(user3)
